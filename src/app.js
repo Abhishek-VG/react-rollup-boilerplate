32a4141bed6f2d92c5e43f1e1f69e4d1fbf92f6a
Abhishek <abhishek.guruswamy@estuate.com>
2019-03-17 13:15:52 +0530

    Added delete script and updated package json file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 
 export default class App extends React.Component {
   render() {
-    return <div className="container">Hello People</div>;
+    const x = () => {console.log('calling india')};
+    x();
+    return (
+      <React.Fragment>
+        <div className="container">Hello People</div>
+      </React.Fragment>
+    );
   }
 }
