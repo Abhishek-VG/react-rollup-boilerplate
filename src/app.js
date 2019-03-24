0f41fe730f7bb381836cc9b0bcaaf9dd0ccd81cd
Abhishek <abhishek.guruswamy@estuate.com>
2019-03-24 12:08:19 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import "./style.css";
 
 export default class App extends React.Component {
   render() {
-    const x = () => {console.log('calling india')};
-    x();
     return (
       <React.Fragment>
         <div className="container">Hello People</div>
